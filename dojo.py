796b217dc2db1f9c3a46a48cf5963886873bc4d8
Ryan Palesano <89495764+ryanpalesano@users.noreply.github.com>
2022-07-23 19:27:09 -0500

    Update dojo.py

--- dojo.py
+++ dojo.py
@@ -4,7 +4,7 @@ from flask_app.models import ninja
 
 class Dojo:
 
-    def __init__(self, ):
+    def __init__(self, data ):
         self.id = data['id']
         self.name = data['name']
         self.created_at = data['created_at']
