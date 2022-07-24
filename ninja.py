45b5ef226fec37728e66e6d45cdf5476cb2395df
Ryan Palesano <89495764+ryanpalesano@users.noreply.github.com>
2022-07-23 19:28:20 -0500

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -4,7 +4,7 @@ from flask_app.models import dojo
 
 class Ninja:
 
-    def __init__(self, ):
+    def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
