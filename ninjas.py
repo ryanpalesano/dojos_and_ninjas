7430bd70590a75e60b11d96158f7003aa3c1136d
Ryan Palesano <89495764+ryanpalesano@users.noreply.github.com>
2022-07-23 19:28:35 -0500

    Update ninjas.py

--- ninjas.py
+++ ninjas.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, request
 from flask_app import app
 from flask_app.models import dojo, ninja
 
-@app.route('/ninjas)
+@app.route('/ninjas')
 def ninjas():
 
     return render_template('ninja.html',dojos= dojo.Dojo.get_all())
